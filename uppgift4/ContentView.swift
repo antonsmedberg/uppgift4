9e431db1d45e576322d93de4c6f273695efb2e1a
antonsmedberg <anton@smedberg.eu>
2023-10-16 11:51:38 +0200

    uppgift 4

--- uppgift4/ContentView.swift
+++ uppgift4/ContentView.swift
@@ -8,17 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var classificationResult: String = ""
+    @State private var isLoading: Bool = false
+    @State private var showError: Bool = false
+
+    private let imageModel = ImageModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 20) {
+                Spacer()
+                ClassifyImageView(imageName: "gorillaios", onClassify: classifyImage)
+                ClassifyImageView(imageName: "flamingoios", onClassify: classifyImage)
+                Spacer()
+
+                if isLoading {
+                    ProgressView("Klassificerar...")
+                        .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                        .scaleEffect(1.5, anchor: .center)
+                } else {
+                    Text(classificationResult)
+                        .font(.largeTitle)
+                        .padding(.all, 10)
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(10)
+                }
+
+                if showError {
+                    Text("Ett fel inträffade vid klassificeringen.")
+                        .foregroundColor(.red)
+                        .padding()
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+
+    func classifyImage(for imageName: String) {
+        isLoading = true
+        showError = false
+
+        do {
+            classificationResult = try imageModel.classifyImage(named: imageName)
+        } catch {
+            showError = true
+            classificationResult = ""
+        }
+
+        isLoading = false
     }
 }
 
-#Preview {
-    ContentView()
+struct ClassifyImageView: View {
+    let imageName: String
+    let onClassify: (String) -> Void
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 10)
+                .padding()
+
+            Button(action: {
+                withAnimation {
+                    onClassify(imageName)
+                }
+            }) {
+                Text("Vilket djur?")
+                    .fontWeight(.bold)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(10)
+                    .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 10)
+            }
+        }
+    }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
--- uppgift4/ImageModel.swift
+++ uppgift4/ImageModel.swift
@@ -5,4 +5,68 @@
 //  Created by Anton Smedberg on 2023-10-16.
 //
 
+import Vision
 import Foundation
+import UIKit
+
+class ImageModel {
+    enum ImageModelError: Error {
+        case modelInitializationFailed
+        case imageLoadingFailed
+        case imageConversionFailed
+        case classificationFailed(String)
+    }
+
+    // Klassificerar en bild baserat på dess namn
+    func classifyImage(named imageName: String) throws -> String {
+        // Försöker initialisera Core ML-modellen
+        guard let imageClassifierWrapper = try? MobileNetV2(configuration: MLModelConfiguration()) else {
+            throw ImageModelError.modelInitializationFailed
+        }
+
+        // Laddar bilden
+        guard let theimage = UIImage(named: imageName) else {
+            throw ImageModelError.imageLoadingFailed
+        }
+
+        // Konverterar bilden till en buffer
+        guard let theimageBuffer = buffer(from: theimage) else {
+            throw ImageModelError.imageConversionFailed
+        }
+
+        do {
+            // Försöker klassificera bilden med hjälp av modellen
+            let output = try imageClassifierWrapper.prediction(image: theimageBuffer)
+            let confidencePercentage = Int((output.classLabelProbs[output.classLabel]! * 100).rounded())
+            let animalName = output.classLabel.components(separatedBy: ",").first?.trimmingCharacters(in: .whitespacesAndNewlines) ?? output.classLabel
+            return "\(animalName) (\(confidencePercentage)%)"
+        } catch {
+            throw ImageModelError.classificationFailed(error.localizedDescription)
+        }
+    }
+
+    // Konverterar en UIImage till en CVPixelBuffer
+    private func buffer(from image: UIImage) -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard status == kCVReturnSuccess else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+        // Ritar bilden i CGContext
+        context?.translateBy(x: 0, y: image.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        UIGraphicsPushContext(context!)
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+
+        return pixelBuffer
+    }
+}
