59a1964da9dbfd33ec60de6b06ede047bb53ea8d
antonsmedberg <anton@smedberg.eu>
2023-10-16 10:24:33 +0200

    Initial Commit

---
+++ uppgift4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uppgift4
+//
+//  Created by Anton Smedberg on 2023-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uppgift4/uppgift4App.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift4App.swift
+//  uppgift4
+//
+//  Created by Anton Smedberg on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct uppgift4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
